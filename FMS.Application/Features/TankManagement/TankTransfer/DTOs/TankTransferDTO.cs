e8a568506f08d148f6010e695fb55e80b21ff86f
Kevin kagiri <kagz100@gmail.com>
2025-10-17 19:22:16 +0300

    Fix : transferdata

--- FMS.Application/Features/TankManagement/TankTransfer/DTOs/TankTransferDTO.cs
+++ FMS.Application/Features/TankManagement/TankTransfer/DTOs/TankTransferDTO.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FMS.Application.Features.FMS.TankTransfer {
-    public class TankTransferDTO {
+namespace FMS.Application.Features.FMS.TankTransfer
+{
+    public class TankTransferDTO
+    {
         public int Id { get; set; }
         public int? SourceTankId { get; set; }
         public int? DestinationTankId { get; set; }
         public decimal? Amount { get; set; }
+        [JsonProperty("transferDate")]
         public DateTime? Date { get; set; }
         public string? RecordedBy { get; set; } = null;
     }
